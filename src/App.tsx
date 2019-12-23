12ce2d4fb2f8e7ae0cf169ce51414203b7c7e2a6
kryptkode <paulakhamiogu@gmail.com>
2019-12-23 23:54:48 +0100

    Create book component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import {BookList} from './components';
+import { BookAPI } from './utils';
 import './App.css';
 
 const App: React.FC = () => {
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    BookAPI.getAll()
+      .then(books => setBooks(books))
+      .catch(error => console.error(error));
+  }, [books]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+       <BookList books={books}/>
     </div>
   );
 }
